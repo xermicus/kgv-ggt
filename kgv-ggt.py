f63b320c9c292ec31d97944e20b299a4e2adf58d
Cyrill Leutwiler <bigcyrill@hotmail.com>
2014-11-15 15:07:07 +0100

    Final version

--- kgv-ggt.py
+++ kgv-ggt.py
@@ -1,4 +1,9 @@
-# Just a simple program in python :-)
+'''
+Dieses simple Programm habe ich geschrieben um den Umgang mit Listen in Python zu erlernen
+Es berechnet das kgV (kleinstes gemeinsames Vielfaches) oder den ggT (grösster gemeinsamer Teiler) von beliebig vielen Zahlen
+Der Input wird validiert
+Der Fokus bei der Programmierung lag auf elegantem Code, die Performance wurde nicht optimiert
+'''
 
 # Hilfsfunktion, berechnet alle Primzahlen <= n
 def primes(n):
@@ -13,111 +18,78 @@ def primes(n):
 	return result
 	
 
-# Berechnet das kgV anhand Primfaktorenzerlegung
-def kgv(a, b):
-	# Berechne die Primzahlen
-	primesa = primes(max(a,b))
-	primesb = primes(max(a,b))
-		
-	# Berechne die Primzahlzerlegung
-	for i in range(0, len(primesa)):
-		while a % primesa[i][0] == 0:
-			primesa[i][1] += 1
-			a /= primesa[i][0]	
-	for i in range(0, len(primesb)):				
-		while b % primesb[i][0] == 0:
-			primesb[i][1] += 1
-			b /= primesb[i][0]
-	
-	# Berechen des Resultats
-	result = 1
-	for i in range(0, len(primesa)):
-		if primesa[i][1] != 0:
-			if primesb[i][1] != 0:
-				result *= primesa[i][0] ** max(primesa[i][1], primesb[i][1])
-			else:
-				result *= primesa[i][0] ** primesa[i][1]
-		elif primesb[i][1] != 0:			
-				result *= primesb[i][0] ** primesb[i][1]
-	
-	return result
-	
-
-# Berechnet den ggT
-def ggt(a, b):
-	while a % b != 0:
-		c = a % b
-		a = b
-		b = c
-		
-	return b
-	
-
-def getnumbers(n):
-	result  = []
-	for i in range(0, n):
-		while True:
-			try:
-				result.append(int(input("Zahl" + str(i+1) + ": ")))
-				break
-			except ValueError:
-				print("Oops! Falsche Eingabe, bitte erneut versuchen.")
-	return result
-	
-	
-# Hauptprogramm
-print("Willkommen zum KGV / GGT Rechner.")
-
-while True:
-	op = str(input("""\nWas gibt es zu tun? 
-Gib k ein für KGV, g für GGT oder etwas anderes um zu beenden: """))
-
-	if op == 'k':
-		val = getnumbers(2)
-		print("Das kgV von {0} und {1} ist: {2}".format(val[0], val[1], kgv(val[0], val[1])))
-		
-	elif str(op) == 'g':
-		val = getnumbers(2)
-		print("Der ggT von {0} und {1} ist: {2}".format(val[0], val[1], ggt(val[0], val[1])))
-		
-	else:
-		print("Goodbye.")
-		break
-	
-
-# Not needed:
-'''def kgv_m(numbers):
+# Berechnet das kgV anhand der Primfaktorenzerlegung
+def kgv(numbers):
 	# Berechne die Primzahlen
 	primz = []
 	for i in range(0, len(numbers)):
 		primz.append(primes(max(numbers)))
 		
-	#primesa = primes(max(a,b))
-	#primesb = primes(max(a,b))
 		
-	# Berechne die Primzahlzerlegung
+	# Berechne die Primfaktorenzerlegung
 	for i in range(0, len(numbers)):
 		for j in range(0, len(primz[i])):
 			while numbers[i] % primz[i][j][0] == 0:
 				primz[i][j][1] += 1
 				numbers[i] /= primz[i][j][0]
 				
-	print(primz)
-	
-	result = 1	
+	# Berechne das Resultat	
+	result = 1		
 	val = []			
-	for i in range(0, len(primz)):
-		for j in range(0, len(primz[0])):
-			val.append(primz[j][i])
-			
-	print(val)
+	for i in range(0, len(primz[0])):
+		val.append([primz[0][i][0], 0])
+		for j in range(0, len(primz)):
+			if primz[j][i][1] > val[i][1]:
+				val[i][1] = primz[j][i][1]
+		result *= val[i][0] ** val[i][1]	
 	
-	for i in range(0, len(val)):
-		result =  result * (val[i][0] ** max(val[i][1]))
+	return result
+
+
+# Berechnet den ggT nach euklidschem Algorithmus
+def ggt(numbers):
+	rest = 0	
+	for i in range(0, len(numbers)-1):
+		while numbers[0] % numbers[1] != 0:
+			rest = numbers[0] % numbers[1]
+			numbers[0] = numbers[1]
+			numbers[1] = rest
+		numbers[0] = numbers[1]
+		numbers[1] = numbers[1+i]
 	
-	print (val)		
+	if rest == 0:
+		return min(numbers)		
+	return rest
 	
-	# Berechen des Resultats
+
+# Helper für den Input
+def getnumbers():
+	result  = []
+	while True:
+		try:
+			result.append(int(input("Zahl: ")))
+		except ValueError as v:
+			if str(v)[-2] == "'":
+				if not result:
+					continue
+				return result
+			print("Oops! Falsche Eingabe, bitte erneut versuchen.")
 	
 	
-	return result'''
+# Hauptprogramm
+print("Willkommen zum kgV / ggT Rechner.")
+
+while True:
+	op = str(input("\nGib k ein für kgV, g für ggT oder etwas anderes um zu beenden: "))
+
+	if op == 'k':
+		print("kgV: Bitte gib ein paar Zahlen ein. Lass die Eingabe leer um fortzufahren.")
+		print("Das kgV deiner Zahlen lautet: " + str(kgv(getnumbers())))
+		
+	elif str(op) == 'g':
+		print("ggT: Bitte gib ein paar Zahlen ein. Lass die Eingabe leer um fortzufahren.")
+		print("Der ggT deiner Zahlen lautet: " + str(ggt(getnumbers())))
+		
+	else:
+		print("Goodbye.")
+		break
